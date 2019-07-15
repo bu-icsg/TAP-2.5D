2c41f6a3cc0a3b41596cf870a567073f7ca5e0cc
Yenai <yenai@scc2.bu.edu>
2019-07-15 14:06:40 -0400

    update config file and config script.

--- config.py
+++ config.py
@@ -1,41 +1,48 @@
 import configparser
-from chiplet import Chiplet
-from system import *
+from system import System_25D
+
+def get_intp_types():
+	return ['passive']
+
+def get_list(option):
+	return list(map(float, ''.join(option.split()).split(',')))
+
+def get_matrix(option):
+	matrix = []
+	for i in ''.join(option.split()).split(';'):
+		matrix.append(list(map(int,i.split(','))))
+	return matrix
 
 def read_config():
 	config = configparser.ConfigParser()
 	config.read('example.cfg')
 
-	path = config.get('taco', 'path')
-	interposer_type = config.get('taco', 'intp_type')
-	assert interposer_type in ['passive'], 'only support for passive interposer so far (to update with active, photonic, and EMIB options)'
-	interposer_size = config.getfloat('taco', 'intp_size')
-	if interposer_type == 'passive':
-		system = System_passive(interposer_size)
+	path = config.get('general', 'path')
 
-	n_chiplet_type = config.getint('2.5D', 'n_chiplet_type')
-	n_chiplet, width_config, height_config, power_config = [0] * n_chiplet_type, [0] * n_chiplet_type, [0] * n_chiplet_type, [0] * n_chiplet_type
-	n_chiplet_total = 0
+	try:
+		granularity = config.getfloat('general', 'pddlacer_granularity')
+	except:
+		granularity = 1.0
 
-	'''read from the simplified config file description'''
-	for i in range(n_chiplet_type):
-		n_chiplet[i] = config.getint('2.5D', 'n_chiplet_'+str(i))
-		n_chiplet_total += n_chiplet[i]
-		width_config[i] = config.getfloat('2.5D', 'width_'+str(i))
-		height_config[i] = config.getfloat('2.5D', 'height_'+str(i))
-		power_config[i] = config.getfloat('2.5D', 'power_'+str(i))
-	system.chiplet_count = n_chiplet_total
+	try:
+		init_place_option = config.get('general', 'initial_placement')
+	except:
+		init_place_option = 'tight'
 
-	'''convert to standard description '''
-	for i in range(n_chiplet_type):
-		for j in range(n_chiplet[i]):
-			system.chiplets.append(Chiplet(width_config[i], height_config[i], power_config[i]))
+	interposer_type = config.get('interposer', 'intp_type')
+	assert interposer_type in get_intp_types(), 'only support for passive interposer so far (to update with active, photonic, and EMIB options)'
+	interposer_size = config.getfloat('interposer', 'intp_size')
 
+	chiplet_count = config.getint('chiplets', 'chiplet_count')
+	chiplet_width = get_list(config.get('chiplets', 'widths'))
+	chiplet_height = get_list(config.get('chiplets', 'heights'))
+	chiplet_power = get_list(config.get('chiplets', 'powers'))
+	chiplet_connection = get_matrix(config.get('chiplets', 'connections'))
+
+	system = System_25D(chiplet_count)
 	return system
 
 if __name__ == "__main__":
 	system = read_config()
-	for i in range(system.chiplet_count):
-		print ('Chiplet', i, system.chiplets[i].width, system.chiplets[i].height, system.chiplets[i].power)
-
+	print (system.chiplet_count, system.power)
 
