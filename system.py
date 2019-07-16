c7043e1085dc840f7e27ac838d1fcea0110b5235
Yenai <yenai@scc2.bu.edu>
2019-07-16 13:53:25 -0400

    common functions

--- system.py
+++ system.py
@@ -1,6 +1,41 @@
-class System_passive:
-	# definiation of 2.5D system object with passive silicon interposer
-	def __init__(self, size):
-		self.intp_size = size
+from init_place_tight import InitPlaceTight
+
+class System_25D:
+	# definiation of 2.5D system object, parent class, common properties and behaviors
+
+	def __init__(self):
 		self.chiplet_count = 0
-		self.chiplets = []
+		self.width = [None] * self.chiplet_count
+		self.height = [None] * self.chiplet_count
+		self.power = [None] * self.chiplet_count
+		self.rotation = [None] * self.chiplet_count
+		self.x = [None] * self.chiplet_count
+		self.y = [None] * self.chiplet_count
+
+	def set_interposer_size(self, intp_size):
+		self.intp_size = intp_size
+
+	def set_chiplet_count(self, chiplet_count):
+		self.chiplet_count = chiplet_count
+
+	def initialize(self):
+		self.rotation = [0] * self.chiplet_count
+		self.width = [None] * self.chiplet_count
+		self.height = [None] * self.chiplet_count
+		self.power = [None] * self.chiplet_count
+		self.x = [None] * self.chiplet_count
+		self.y = [None] * self.chiplet_count		
+
+	def set_chiplet_size(self, width, height):
+		self.width = width
+		self.height = height
+
+	def set_chiplet_power(self, power):
+		self.power = power
+
+	def set_connection_matrix(self, connection):
+		self.connection_matrix = connection
+
+	def initial_placement(self, init_place_option):
+		if init_place_option == 'tight':
+			InitPlaceTight(self.intp_size, self.chiplet_count, self.width, self.height)
