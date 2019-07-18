3053b8871a66df8c272bc703301b2fb7da2fd220
yenai <yenai@bu.edu>
2019-07-18 13:44:39 -0400

    update test script

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ import config
 if __name__ == "__main__":
 	# read config file and initialize the parameters
 	system = config.read_config()
-	print (system.chiplet_count, system.intp_size, system.power)
-	system.initial_placement('tight')
-	# initialize the placement
+	print (system.chiplet_count, system.intp_size, system.power, system.x)
+	
+	system.gen_flp('test')
