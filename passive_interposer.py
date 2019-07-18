89b02164a6ec538ebf8ae14c8314a7c1077ce021
yenai <yenai@bu.edu>
2019-07-18 14:30:51 -0400

    working on gen_flp, for fill_space, call module and pass args instead of os.system

--- passive_interposer.py
+++ passive_interposer.py
@@ -1,4 +1,6 @@
 from system import System_25D
+import os
+import util.fill_space
 
 class PassiveInterposer(System_25D):
 	"""docstring for Passive"""
@@ -42,15 +44,59 @@ class PassiveInterposer(System_25D):
 			L0_Substrate.write("# comment lines begin with a '#' \n")
 			L0_Substrate.write("# comments and empty lines are ignored\n\n")
 			L0_Substrate.write("Substrate\t"+str(self.intp_size/1000)+"\t"+str(self.intp_size/1000)+"\t0.0\t0.0\n")
-
 		os.system("perl util/tofig.pl -f 20 "+self.path+filename+"L0_Substrate.flp | fig2dev -L ps | ps2pdf - "+self.path+filename+"L0_Substrate.pdf")
+
+		with open(self.path+filename +'L1_C4Layer.flp','w') as L1_C4Layer:
+			L1_C4Layer.write("# Floorplan for C4 Layer \n")
+			L1_C4Layer.write("# Line Format: <unit-name>\\t<width>\\t<height>\\t<left-x>\\t<bottom-y>\\t[<specific-heat>]\\t[<resistivity>]\n")
+			L1_C4Layer.write("# all dimensions are in meters\n")
+			L1_C4Layer.write("# comment lines begin with a '#' \n")
+			L1_C4Layer.write("# comments and empty lines are ignored\n\n")
+			L1_C4Layer.write("C4Layer\t"+str(self.intp_size / 1000)+"\t"+str(self.intp_size / 1000)+"\t0.0\t0.0"+mat_C4)
+		os.system("perl util/tofig.pl -f 20 "+self.path+filename+"L1_C4Layer.flp | fig2dev -L ps | ps2pdf - "+self.path+filename+"L1_C4Layer.pdf")
+
+		with open(self.path+filename +'L2_Interposer.flp','w') as L2_Interposer:
+			L2_Interposer.write("# Floorplan for Silicon Interposer Layer\n")
+			L2_Interposer.write("# Line Format: <unit-name>\\t<width>\\t<height>\\t<left-x>\\t<bottom-y>\\t[<specific-heat>]\\t[<resistivity>]\n")
+			L2_Interposer.write("# all dimensions are in meters\n")
+			L2_Interposer.write("# comment lines begin with a '#' \n")
+			L2_Interposer.write("# comments and empty lines are ignored\n\n")
+			L2_Interposer.write("Interposer\t"+str(self.intp_size / 1000)+"\t"+str(self.intp_size / 1000)+"\t0.0\t0.0"+mat_TSV)
+		os.system("perl util/tofig.pl -f 20 "+self.path+filename+"L2_Interposer.flp | fig2dev -L ps | ps2pdf - "+self.path+filename+"L2_Interposer.pdf")
+
+		with open(self.path+filename + 'sim.flp','w') as SIMP:
+			with open(self.path + filename + 'L3.flp', 'w') as L3_UbumpLayer:
+				L3_UbumpLayer.write("# Floorplan for Microbump Layer \n")
+				L3_UbumpLayer.write("# Line Format: <unit-name>\\t<width>\\t<height>\\t<left-x>\\t<bottom-y>\\t[<specific-heat>]\\t[<resistivity>]\n")
+				L3_UbumpLayer.write("# all dimensions are in meters\n")
+				L3_UbumpLayer.write("# comment lines begin with a '#' \n")
+				L3_UbumpLayer.write("# comments and empty lines are ignored\n\n")
+				x_offset0, y_offset0 = 0, 0
+				index_ubump = 0
+				for i in range(0, self.chiplet_count):
+					x_offset1 = x_offset0 + self.x[i] / 1000 - self.width[i] / 1000 * 0.5
+					y_offset1 = y_offset0 + self.y[i] / 1000 - self.height[i] / 1000 * 0.5
+					if self.ubump > 0:
+						L3_UbumpLayer.write("Ubump_"+str(index_ubump)+"\t"+str(self.width[i] / 1000 - self.ubump / 1000)+"\t"+str(self.ubump / 1000)+"\t"+str(x_offset1)+"\t"+str(y_offset1)+mat_ubump)
+						L3_UbumpLayer.write("Ubump_"+str(index_ubump+1)+"\t"+str(self.ubump / 1000)+"\t"+str(self.height[i] / 1000 - self.ubump / 1000)+"\t"+str(x_offset1)+"\t"+str(y_offset1+self.ubump / 1000)+mat_ubump)
+						L3_UbumpLayer.write("Ubump_"+str(index_ubump+2)+"\t"+str(self.ubump / 1000)+"\t"+str(self.height[i] / 1000 - self.ubump / 1000)+"\t"+str(x_offset1+self.width[i] / 1000 - self.ubump / 1000)+"\t"+str(y_offset1)+mat_ubump)
+						L3_UbumpLayer.write("Ubump_"+str(index_ubump+3)+"\t"+str(self.width[i] / 1000 - self.ubump / 1000)+"\t"+str(self.ubump / 1000)+"\t"+str(x_offset1+self.ubump / 1000)+"\t"+str(y_offset1+self.height[i] / 1000 - self.ubump / 1000)+mat_ubump)
+					index_ubump += 4
+					# not sure about the microbump density for the center region. Assume the same as the edge area so far. Need to be updated if the microbump pitch for center power/gnd clk is found
+					L3_UbumpLayer.write("Chiplet_"+str(i)+"\t"+str(self.width[i] / 1000 - self.ubump / 1000 * 2)+"\t"+str(self.height[i] / 1000 - self.ubump / 1000 * 2)+"\t"+str(x_offset1 + self.ubump / 1000)+"\t"+str(y_offset1+self.ubump / 1000)+mat_ubump)
+					
+					SIMP.write("Unit_"+str(i)+"\t"+str(self.width[i] / 1000)+"\t"+str(self.height[i] / 1000)+"\t"+str(x_offset1)+"\t"+str(y_offset1)+mat_ubump)
+		os.system("perl util/tofig.pl -f 20 "+self.path+filename+"L3.flp | fig2dev -L ps | ps2pdf - "+self.path+filename+"L3.pdf")
+
+		util.fill_space.fill_space(x_offset0,x_offset0+self.intp_size / 1000, y_offset0, y_offset0+self.intp_size / 1000, self.path+filename+'sim', self.path+filename+'L3', self.path+filename+'L3_UbumpLayer')
+		os.system("perl util/tofig.pl -f 20 "+self.path+filename+"L3_UbumpLayer.flp | fig2dev -L ps | ps2pdf - "+self.path+filename+"L3_UbumpLayer.pdf")
+
+
+
 
 
 	# print floorplans into pdf files
 	# print "plotting floorplans......................."
-	# os.system("perl tofig.pl -f 20 "+path+filename+"L1_C4Layer.flp | fig2dev -L ps | ps2pdf - "+path+filename+"L1_C4Layer.pdf")
-	# os.system("perl tofig.pl -f 20 "+path+filename+"L2_Interposer.flp | fig2dev -L ps | ps2pdf - "+path+filename+"L2_Interposer.pdf")
-	# os.system("perl tofig.pl -f 4 "+path+filename+"L3_UbumpLayer.flp | fig2dev -L ps | ps2pdf - "+path+filename+"L3_UbumpLayer.pdf")
 	# os.system("perl tofig.pl -f 4 "+path+filename+"L3.flp | fig2dev -L ps | ps2pdf - "+path+filename+"L3.pdf")
 	# os.system("perl tofig.pl -f 4 "+path+filename+"L4_Cores.flp | fig2dev -L ps | ps2pdf - "+path+filename+"L4_Cores.pdf")
 	# os.system("perl tofig.pl -f 4 "+path+filename+"L4.flp | fig2dev -L ps | ps2pdf - "+path+filename+"L4.pdf")
