3307c2a99b6e055be2570066fa77f3083c69538e
yenai <yenai@bu.edu>
2019-07-31 16:56:40 -0400

    add commandline options to change some parameters (path, config-filename, interposer size so far).

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import configparser
-import os
+import getopt
+import os, sys
 from passive_interposer import PassiveInterposer
 
 def get_intp_types():
@@ -15,10 +16,33 @@ def get_matrix(option):
 	return matrix
 
 def read_config():
-	config = configparser.ConfigParser()
-	config.read('example.cfg')
+	opts = parse_command()
 
-	path = config.get('general', 'path')
+	# default config filename
+	filename = 'example.cfg'
+	path = None
+	overwrite = []
+	interposer_size = 0
+	for o, a in opts:
+		if o == '-h':
+			usage()
+			sys.exit()
+		if o == '-c':
+			filename = a
+		if o == '-g':
+			overwrite.append(a.split('='))
+		if o == '-d':
+			path = a
+	for o, a in overwrite:
+		# for -g right now I only expected to change intp_size, will add more options later if necessary
+		if o == 'intp_size':
+			interposer_size = float(a)
+
+	config = configparser.ConfigParser()
+	config.read(filename)
+
+	if not path:
+		path = config.get('general', 'path')
 
 	try:
 		granularity = config.getfloat('general', 'placer_granularity')
@@ -40,7 +64,8 @@ def read_config():
 	chiplet_connection = get_matrix(config.get('chiplets', 'connections'))
 
 	if interposer_type == 'passive':
-		interposer_size = config.getfloat('interposer', 'intp_size')
+		if interposer_size == 0:
+			interposer_size = config.getfloat('interposer', 'intp_size')
 		system = PassiveInterposer()
 		system.set_path(path)
 		if os.path.exists(path) == False:
@@ -53,9 +78,22 @@ def read_config():
 		system.set_connection_matrix(chiplet_connection)
 		system.set_granularity(granularity)
 		system.initial_placement(init_place_option)
+
 	return system
+
+def usage():
+	print ('Usage:')
+	print ('%s -d <outputdir> -c <config-file> -g <options>' % sys.argv[0])
+	sys.exit(2)
+
+def parse_command():
+	try:
+		opts, args = getopt.getopt(sys.argv[1:], "hc:d:g:")
+	except getopt.GetoptError as err:
+		print (err)
+		usage()
+	return opts
 
 if __name__ == "__main__":
 	system = read_config()
 	print (system.chiplet_count, system.intp_size, system.power, system.x)
-
