8b028bc016fb4c1d57728030c5ce0bdf2ebe90d5
yenai <yenai@bu.edu>
2019-09-19 14:56:30 -0400

    modify system structure to include an array of microbump overhead, add a function to compute the overhead, and update the chiplet width and height to include this overhead before calling init_placement function

--- system.py
+++ system.py
@@ -7,6 +7,7 @@ class System_25D:
 		self.chiplet_count = 0
 		self.width = [None] * self.chiplet_count
 		self.height = [None] * self.chiplet_count
+		self.hubump = [None] * self.chiplet_count
 		self.power = [None] * self.chiplet_count
 		self.rotation = [None] * self.chiplet_count
 		self.x = [None] * self.chiplet_count
@@ -25,6 +26,7 @@ class System_25D:
 		self.rotation = [0] * self.chiplet_count
 		self.width = [None] * self.chiplet_count
 		self.height = [None] * self.chiplet_count
+		self.hubump = [None] * self.chiplet_count
 		self.power = [None] * self.chiplet_count
 		self.x = [None] * self.chiplet_count
 		self.y = [None] * self.chiplet_count
@@ -47,8 +49,12 @@ class System_25D:
 		self.width[i], self.height[i] = self.height[i], self.width[i]
 
 	def initial_placement(self, init_place_option, xx, yy):
+		new_width, new_height = [None] * self.chiplet_count, [None] * self.chiplet_count
+		for i in range(self.chiplet_count):
+			new_width[i] = self.width[i] + 2 * self.hubump[i]
+			new_height[i] = self.height[i] + 2 * self.hubump[i]
 		if init_place_option == 'tight':
-			x, y, rotation = init_placement.init_place_tight(self.intp_size, self.granularity, self.chiplet_count, self.width, self.height)
+			x, y, rotation = init_placement.init_place_tight(self.intp_size, self.granularity, self.chiplet_count, new_width, new_height)
 			self.x = x
 			self.y = y
 			self.rotation = rotation
@@ -64,3 +70,23 @@ class System_25D:
 
 	def run_hotspot(self):
 		pass
+
+	def compute_ubump_overhead(self):
+		connection = self.connection_matrix
+		for i in range(self.chiplet_count):
+			assert connection[i][i] == 0, 'a link from and to the same chiplet is not allowed'
+			s = 0
+			for j in range(self.chiplet_count):
+				s += connection[i][j] + connection[j][i]
+			h = 1
+			w_stretch = 0.045 * h
+			while ((self.width[i] + self.height[i]) * 2 * w_stretch + 4 * w_stretch * w_stretch) / 0.045 / 0.045 < s:
+				h += 1
+				w_stretch = 0.045 * h
+				if h > 1000:
+					print ('microbump is too high to be a feasible case')
+					exit()
+			# print (i, s, self.width[i], self.height[i], h, w_stretch)
+			self.hubump[i] = w_stretch
+		print (self.hubump)
+
