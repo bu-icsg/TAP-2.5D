9edb2c8b26d3528eb30d044aacd44d07275067eb
yenai <yenai@bu.edu>
2019-09-24 16:39:44 -0400

    update qsub script

--- qsub.py
+++ qsub.py
@@ -3,18 +3,21 @@ import os
 
 
 cases = ['40', '50', 'small']
+link_types = ['nppl','ppl']
 iterations = 5
 
 for c in cases:
-	for i in range(iterations):
-		with open ('run_'+c+'_'+str(i)+'.sh', 'w') as RUN:
-			RUN.write('#!/bin/bash -l\n')
-			RUN.write('#$ -N run_'+c+'_'+str(i)+'\n#$ -j y \n\n')
-			RUN.write('module load python3/3.6.5\n')
-			RUN.write('time python sim_annealing.py -d outputs/rotation/run_'+c+'_'+str(i)+'/ ')
-			if c == '50':
-				RUN.write('-g intp_size=50 ')
-			elif c == 'small':
-				RUN.write('-c small_chiplet.cfg ')
-			RUN.write('\n')
-		os.system('qsub run_'+c+'_'+str(i)+'.sh')
+	for ltype in link_types:
+		for i in range(iterations):
+			with open ('run_'+c+'_'+ltype+str(i)+'.sh', 'w') as RUN:
+				RUN.write('#!/bin/bash -l\n')
+				RUN.write('#$ -N run_'+c+'_'+ltype+str(i)+'\n#$ -j y \n\n')
+				RUN.write('module load python3/3.6.5\n')
+				RUN.write('module load cplex/12.8_ac\n')
+				RUN.write('time python sim_annealing.py -d outputs/Sep24/run_'+c+'_'+ltype+str(i)+'/ -g link_type='+ltype+' ')
+				if c == '50':
+					RUN.write('-g intp_size=50 ')
+				elif c == 'small':
+					RUN.write('-c small_chiplet.cfg ')
+				RUN.write('\n')
+			os.system('qsub run_'+c+'_'+ltype+str(i)+'.sh')
