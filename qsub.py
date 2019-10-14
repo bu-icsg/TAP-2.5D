153b7c65274a05736a74cdb44936d33fc8b9fe7a
yenai <yenai@bu.edu>
2019-10-14 14:02:03 -0400

    minor tweak in script for submitting donut and ring network

--- qsub.py
+++ qsub.py
@@ -2,22 +2,25 @@
 import os
 
 
-cases = ['40', '50', 'small']
+# cases = ['40', '50', 'small', 'homo_ring']
+cases = ['homo_ring','homo_donut']
 link_types = ['nppl','ppl']
-iterations = 5
+multi_start = 10
 
 for c in cases:
 	for ltype in link_types:
-		for i in range(iterations):
+		for i in range(multi_start):
 			with open ('run_'+c+'_'+ltype+str(i)+'.sh', 'w') as RUN:
 				RUN.write('#!/bin/bash -l\n')
 				RUN.write('#$ -N run_'+c+'_'+ltype+str(i)+'\n#$ -j y \n\n')
 				RUN.write('module load python3/3.6.5\n')
 				RUN.write('module load cplex/12.8_ac\n')
-				RUN.write('time python sim_annealing.py -d outputs/Sep24/run_'+c+'_'+ltype+str(i)+'/ -g link_type='+ltype+' ')
+				RUN.write('time python sim_annealing.py -d outputs/Oct07/run_'+c+'_'+ltype+str(i)+'/24/ -g link_type='+ltype+' ')
 				if c == '50':
 					RUN.write('-g intp_size=50 ')
 				elif c == 'small':
 					RUN.write('-c small_chiplet.cfg ')
+				elif c == 'homo_ring' or c == 'homo_donut':
+					RUN.write('-c '+c+'.cfg')
 				RUN.write('\n')
-			os.system('qsub run_'+c+'_'+ltype+str(i)+'.sh')
+			os.system('qsub -l h_rt=24:00:00 -o run_'+c+'_'+ltype+str(i)+'.o run_'+c+'_'+ltype+str(i)+'.sh')
