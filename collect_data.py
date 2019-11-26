bb93ae8666b33e6c424f74bff5359c9729dbbc78
yenai <yenai@bu.edu>
2019-11-26 10:40:22 -0500

    give simulation status

--- collect_data.py
+++ collect_data.py
@@ -2,13 +2,15 @@
 import os
 
 start_points = range(10)
-cases = ['ascend910','multigpu','micro150','micro125','micro100','micro75','micro50']
+cases = ['ascend910','multigpu','micro150','micro125','micro100','micro75','micro50']  #,'topo_a','topo_b','topo_c','topo_d'
 link_types = ['nppl', 'ppl']
-weights = ['equal', 'adpT', 'adpTW']		
-intp_sizes = [50]		 # and 40
+weights = ['adpTWv2']		# 'equal', 'adpT', 'adpTW', 
+intp_sizes = [50, 45]		 # and 40
 decay_factors = [0.8, 0.85, 0.9, 0.95]    # and 0.95
 
 def check_status():
+	yes = 0
+	pending = 0
 	with open ('outputs/Nov21/status.csv', 'w') as STATUS:
 		STATUS.write('Case, link_type, Weight, Decay, intp_size, start_point, Finish?, complete_step, best_step, temp, length\n')
 		for c in cases:
@@ -21,6 +23,7 @@ def check_status():
 								run_name = c+'_'+ltype+'_'+weight + '_'+str(decay)+'_'+str(intp_size)+'_'+str(i)
 								if os.path.isfile(path + 'output.txt'):
 									finish = 'Y'
+									yes = yes + 1
 								else:
 									finish = 'N'
 								if os.path.isfile(path + 'log.txt'):
@@ -35,13 +38,15 @@ def check_status():
 											y_best = list(map(float,LOG.readline()[1:-2].split(',')))
 											ln = LOG.readline()
 									towrite = ','.join([c, ltype, weight, str(decay), str(intp_size), str(i), finish, str(step), str(step_best), str(temp_best), str(length_best)])
-									print (towrite)
+									# print (towrite)
 									STATUS.write(towrite+'\n')
+									pending += 1
 								else:
 									towrite = ','.join([c, ltype, weight, str(decay), str(intp_size), str(i), finish])
 									STATUS.write(towrite+'\n')
-									print (towrite)
-
+									# print (towrite)
+	print ('finished: ', yes)
+	print ('running:  ', pending - yes)
 
 def clean():
 	for c in cases:
@@ -64,10 +69,10 @@ def clean():
 								# 	CLEAN.write('rm -r '+path+'*\n')
 								# 	CLEAN.write('rm run_'+run_name+'.*\n')
 								# os.system('qsub -j y clean_' + run_name + '.sh -j y')
-								# pass
-								if os.path.isfile('run_' + run_name +'.sh'):
-									os.system('rm -r ' + path + '*')
-									os.system('rm run_'+run_name+'.*')
+								pass
+								# if os.path.isfile('run_' + run_name +'.sh'):
+								# 	os.system('rm -r ' + path + '*')
+								# 	os.system('rm run_'+run_name+'.*')
 							# else:
 								# with open('clean_' + run_name + '.sh', 'w') as CLEAN:
 								# 	CLEAN.write('rm '+path+'{*.flp,*.lcf,*.ptrace,*.steady,*ChipLayer.pdf}\n')
