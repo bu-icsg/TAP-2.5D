68b400029c3d070a26e3506c733dcc093a3cecf1
yenai <yenai@bu.edu>
2019-12-17 22:40:48 -0500

    update qsub.py script for collecting ML data

--- qsub.py
+++ qsub.py
@@ -5,10 +5,12 @@ def qsub():
 	if len(sys.argv)>1:
 		start_point = int(sys.argv[1])
 	else:
-		start_point = 12
+		start_point = 0
 
+	num_systems = 100
 	n = 0
-	for c in cases:
+	for syst in range(3):
+		c = 'rand' + str(syst)
 		for ltype in link_types:
 			for weight in weights:
 				for decay in decay_factors:
@@ -17,14 +19,15 @@ def qsub():
 						path = 'outputs/Dec2019/' + c + '/' + ltype + '/' + weight + '/' + str(decay) + '/' + str(intp_size) + '/' + str(i) + '/'
 						run_name = c+'_'+ltype+'_'+weight + '_'+str(decay)+'_'+str(intp_size)+'_'+str(i)
 						print (path)
-						if os.path.isfile('run_'+run_name + '.sh') == False:
-							with open ('run_'+run_name+'.sh', 'w') as RUN:
+						os.system('mkdir -p ' + path)
+						if os.path.isfile(path + 'run_'+run_name + '.sh') == False:
+							with open (path + 'run_'+run_name+'.sh', 'w') as RUN:
 								RUN.write('#!/bin/bash -l\n')
 								RUN.write('#$ -N run_'+run_name+'\n#$ -j y \n\n')
 								RUN.write('module load python3/3.6.5\n')
 								RUN.write('module load cplex/12.8_ac\n')
 								RUN.write('time python sim_annealing.py -d '+path +' -c configs/sys_'+c+'.cfg -g link_type='+ltype+' -g weight='+weight+' -g decay='+str(decay)+' -g intp_size='+str(intp_size)+'\n')
-							os.system('qsub -l h_rt=100:00:00 -o run_'+run_name+'.o run_'+run_name+'.sh')
+							os.system('qsub -l h_rt=100:00:00 -o '+path + 'run_'+run_name+'.o ' + path + 'run_'+run_name+'.sh')
 							n += 1
 	print ('submit ', n, ' new jobs')
 
@@ -55,12 +58,15 @@ def random_sys_generator():
 						num_conn = random.randint(chiplet_count, int(chiplet_count*chiplet_count / 2))
 						for i in range(num_conn):
 							s, t = random.randint(0, chiplet_count - 1), random.randint(0, chiplet_count - 1)
-							bw = random.randint(0, 8) * 128
+							while s == t:
+								s, t = random.randint(0, chiplet_count - 1), random.randint(0, chiplet_count - 1)
+							bw = random.randint(1, 8) * 128
 							connections[s][t] = bw
 							connections[t][s] = bw
 
 						path = 'configs/'
-						if os.path.isfile(path + 'sys_'+c + '.cfg') == False:
+						# if os.path.isfile(path + 'sys_'+c + '.cfg') == False:
+						if True:
 							with open (path + 'sys_'+c+'.cfg', 'w') as CONFIG:
 								CONFIG.write('[general]\n')
 								CONFIG.write('path = /projectnb/photonoc/yenai/hetero-placer/Dec2019/\n')
@@ -75,17 +81,18 @@ def random_sys_generator():
 								CONFIG.write('chiplet_count = '+ str(chiplet_count)+'\n')
 								CONFIG.write('widths = '+','.join(map(str, widths))+'\n')
 								CONFIG.write('heights = '+','.join(map(str, heights))+'\n')
-								CONFIG.write('power = '+','.join(map(str, powers))+'\n')
+								CONFIG.write('powers = '+','.join(map(str, powers))+'\n')
 								CONFIG.write('connections = ')
 								for i in range(chiplet_count-1):
-									CONFIG.write(','.join(map(str, connections[i])) + ';\n')
-								CONFIG.write(','.join(map(str, connections[chiplet_count - 1]))+'\n')
+									CONFIG.write('\t'+','.join(map(str, connections[i])) + ';\n')
+								CONFIG.write('\t'+','.join(map(str, connections[chiplet_count - 1]))+'\n')
 
 cases = ['ascend910','multigpu','micro150','micro125','micro100','micro75']   #,'topo_a','topo_b','topo_c','topo_d'
 link_types = ['nppl', 'ppl']
 weights = ['adpTWv2']		# not going to run adpT, not perform well. adpTWh also covers the same concept as adpTW, but amend the issue in low temp region. 'equal',
 intp_sizes = [50, 45]		 # and 40
-decay_factors = [0.9, 0.95]    # and 0.95
+decay_factors = [0.8, 0.85, 0.9, 0.95]    # and 0.95
 
-# qsub()
-random_sys_generator()
+qsub()
+# random_sys_generator()
+# autocheck()
