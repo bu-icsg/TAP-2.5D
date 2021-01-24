6521b2156a2c0a29f0470a85d87e916234c93f95
yenai <yenai@bu.edu>
2021-01-24 17:15:56 -0500

    remove unused file

--- main.py
+++
@@ -1,9 +0,0 @@
-import config
-
-
-if __name__ == "__main__":
-	# read config file and initialize the parameters
-	system = config.read_config()
-	system.gen_flp('test')
-	system.gen_ptrace('test')
-	print (system.run_hotspot('test'))
